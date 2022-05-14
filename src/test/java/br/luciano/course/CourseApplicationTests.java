e008aa7ddb586446bafc5325580fd2ff1e573027
Luciano Mendonca <luciano.s.mend@gmail.com>
2022-05-14 16:04:32 -0300

    Criação do projeto

---
+++ src/test/java/br/luciano/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.luciano.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
