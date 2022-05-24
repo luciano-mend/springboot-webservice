8f83b1816c75f67a92396ca53b9e7f3abc0b2eeb
Luciano Mendonca <luciano.s.mend@gmail.com>
2022-05-23 21:12:51 -0300

    Entidade Order, tipo de dado Instant e Join de entidades

---
+++ src/main/java/br/luciano/course/entities/Order.java
@@ -0,0 +1,82 @@
+package br.luciano.course.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_order")
+public class Order implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long Id;
+	private Instant moment;
+	
+	@ManyToOne
+	@JoinColumn(name="client_id")
+	private User client;
+	
+	public Order() {
+	}
+
+	public Order(long id, Instant moment, User client) {
+		super();
+		Id = id;
+		this.moment = moment;
+		this.client = client;
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Id == other.Id;
+	}
+	
+	
+}
