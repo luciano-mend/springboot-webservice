60a0e5051574d5c0f07c83401beecf4cbf332a53
Luciano Mendonca <luciano.s.mend@gmail.com>
2022-05-28 14:04:05 -0300

    Criado entidade, repository, service e resource de Product

--- src/main/java/br/luciano/course/config/TestConfig.java
+++ src/main/java/br/luciano/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import br.luciano.course.entities.Category;
 import br.luciano.course.entities.Order;
+import br.luciano.course.entities.Product;
 import br.luciano.course.entities.User;
 import br.luciano.course.entities.enums.OrderStatus;
 import br.luciano.course.repositories.CategoryRepository;
 import br.luciano.course.repositories.OrderRepository;
+import br.luciano.course.repositories.ProductRepository;
 import br.luciano.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -36,7 +41,15 @@ public class TestConfig implements CommandLineRunner{
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "O Senhor dos Anéis é uma trilogia cinematográfica dirigida por Peter Jackson com base na obra-prima homónima de J. R. R. Tolkien.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Toda televisão que se conecta à Internet é chamada Smart TV.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "The MacBook Pro is the top line of MacBook laptops, introduced in 2006.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "PC gamer nada mais é do que um computador voltado para jogos.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Quickly create Web sites with this powerful tool.", 100.99, "");
+		
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		User u1 = new User(null, "Joana D A Brown", "joanadarc@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Elizabeth Green", "lizag@gmail.com", "977777777", "123456");
---
+++ src/main/java/br/luciano/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package br.luciano.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.luciano.course.entities.Product;
+import br.luciano.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
