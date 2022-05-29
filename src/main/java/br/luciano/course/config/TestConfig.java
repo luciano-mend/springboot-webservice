b8e95e4986b58f013e19a12dd9dc0e7fe6a71ecd
Luciano Mendonca <luciano.s.mend@gmail.com>
2022-05-29 11:41:43 -0300

    Criado relacionamento um para um entre Order e Payment

--- src/main/java/br/luciano/course/config/TestConfig.java
+++ src/main/java/br/luciano/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.luciano.course.entities.Category;
 import br.luciano.course.entities.Order;
 import br.luciano.course.entities.OrderItem;
+import br.luciano.course.entities.Payment;
 import br.luciano.course.entities.Product;
 import br.luciano.course.entities.User;
 import br.luciano.course.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
