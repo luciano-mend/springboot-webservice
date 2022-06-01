ca05c5f74b52a3ab30ea71f698c994b1f8ca1e0e
Luciano Mendonca <luciano.s.mend@gmail.com>
2022-05-31 21:32:37 -0300

    Tratamento de exceção findById

--- src/main/java/br/luciano/course/services/UserService.java
+++ src/main/java/br/luciano/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.luciano.course.entities.User;
 import br.luciano.course.repositories.UserRepository;
+import br.luciano.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
